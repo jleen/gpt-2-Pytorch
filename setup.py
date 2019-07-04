a52135f9c5bebcdc0ecc13e0f045fd573b1c604f
John Leen <jleen@saturnvalley.org>
2019-07-04 16:44:23 -0400

    Data files

--- setup.py
+++ setup.py
@@ -3,4 +3,5 @@ from setuptools import setup
 setup(
     name='gpt2pytorch',
     version='0.1',
-    packages=['GPT2'])
+    packages=['GPT2'],
+    package_data={'GPT2': ['encoder.json', 'vocab.bpe']})
